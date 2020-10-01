1f05b6c66ff6d98ecd40603a71de693c2897e68c
Ankitsharma0001 <ankitbhardoaj@gmail.com>
2020-10-01 08:33:39 +0530

    Updates

---
+++ precache-manifest.49bcb6b8b000567629382164edf7d8de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe87cade0a090fffdb7da04650cd0a4",
+    "url": "/monsters-hunt/index.html"
+  },
+  {
+    "revision": "66508c03ed3ef8a37969",
+    "url": "/monsters-hunt/static/css/main.499d4cf1.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/monsters-hunt/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "66508c03ed3ef8a37969",
+    "url": "/monsters-hunt/static/js/main.1abde8cb.chunk.js"
+  },
+  {
+    "revision": "6420dea6e180647b724e",
+    "url": "/monsters-hunt/static/js/runtime~main.39bef957.js"
+  }
+]);
